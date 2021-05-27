d73b8519dac2d37b31e213a018f7362dce08d2d5
Red Crossbones <gastonnehuen@outlook.com>
2021-05-27 16:34:59 -0300

    Entrega de Ej.2 [paliza]

--- tp4-ej2.py
+++ tp4-ej2.py
@@ -16,39 +16,37 @@ if __name__ == "__main__":
 
 def suma_lenta():
     suma = 0
-    suma2=0
+    suma_en_pantalla=0
     numero_menor=0
     print("SUMA LENTA\n")
 
 #Titulo
-
     numero = int(input("Ingrese el numero A: "))
     suma = numero
     otro_numero= int(input("Ingrese el numero B: "))
     suma=numero+otro_numero
 
 #Aca se viene lo jodido
-
     if numero > otro_numero:
         numero_menor=otro_numero
         print(f"\nEmpezamos desde:  {numero_menor}")
     else:
         numero_menor=numero
         print(f"\nEmpezamos desde:  {numero_menor}")
+
 #Eso no. Esto:
-
-
     if suma < 0 and (numero < 0 > otro_numero):
-        while suma2 != suma:
-            suma2 = numero_menor - 1
-            print(f"Resultado de suma lenta: {numero_menor} - 1  =  {suma2}")
+        while suma_en_pantalla != suma:
+            suma_en_pantalla = numero_menor - 1
+            print(f"{numero_menor} - 1  =  {suma_en_pantalla}")
             numero_menor=numero_menor-1
+    elif (numero == 0 or otro_numero == 0):
+        print(f" {numero_menor} + {otro_numero}  = {suma_en_pantalla}")
     else:
-        while suma2 != suma:
-            suma2 = numero_menor + 1
-            print(f"Resultado de suma lenta: {numero_menor} + 1  = {suma2}")
+        while suma_en_pantalla != suma:
+            suma_en_pantalla = numero_menor + 1
+            print(f"{numero_menor} + 1  = {suma_en_pantalla}")
             numero_menor=numero_menor+1
-
-#Estoy tan orgulloso de esta nueva versión, que es como un hijo. Se llama Mantra. Le gusta los números.
+            
     print(f"El resultado final es:  {suma}")
 suma_lenta()
