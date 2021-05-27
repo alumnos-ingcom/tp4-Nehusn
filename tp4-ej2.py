22c513d9aa6f8e4279c9089732b1881310321699
Red Crossbones <gastonnehuen@outlook.com>
2021-05-27 00:48:53 -0300

    Entrega de Ej.2 [aplausos]

--- tp4-ej2.py
+++ tp4-ej2.py
@@ -1,3 +1,19 @@
+################
+# Martín René - @martinvilu
+# Plantilla de ejercicio
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+
+# Reemplazar por las funciones del ejercicio
+
+
+def prueba():
+    pass
+
+if __name__ == "__main__":
+    prueba()
+
 def suma_lenta():
     suma = 0
     suma2=0
