f24b93a2736fc78ae6c58e99faba4c0181b46950
Red Crossbones <gastonnehuen@outlook.com>
2021-05-27 17:59:05 -0300

    Entrega pulida Ej.2 [fatality]

--- tp4-ej2.py
+++ tp4-ej2.py
@@ -10,11 +10,11 @@ def prueba():
     suma_lenta(numero, otro_numero)
 
 def suma_lenta(numero, otro_numero):
-    suma = 0
     suma_en_pantalla=0
     numero_menor=0
-    suma=numero+otro_numero
-#Aca se viene lo jodido
+    resultado_suma=numero+otro_numero
+#Codigo para elegir un numero menor y empezar a sumar desde ahi
+
     if numero > otro_numero:
         numero_menor=otro_numero
         print(f"\nEmpezamos desde:  {numero_menor}")
@@ -22,21 +22,32 @@ def suma_lenta(numero, otro_numero):
         numero_menor=numero
         print(f"\nEmpezamos desde:  {numero_menor}")
 
-#Eso no. Esto:
-    if suma < 0 and (numero < 0 > otro_numero):
-        while suma_en_pantalla != suma:
+#Loop para mostar y demostar en pantalla:
+
+#Si es una suma de dos negativos
+        
+    if resultado_suma < 0 and (numero < 0 > otro_numero):
+        while suma_en_pantalla != resultado_suma:
             suma_en_pantalla = numero_menor - 1
-            print(f"{numero_menor} - 1  =  {suma_en_pantalla}")
+            print(f"{numero_menor} + (-1)  =  {suma_en_pantalla}")
             numero_menor=numero_menor-1
+            
+#Si uno de los numeros o ambos es cero
+            
     elif (numero == 0 or otro_numero == 0):
         print(f" {numero_menor} + {otro_numero}  = {suma_en_pantalla}")
+        
+#Basicamente cualquir otra cosa
+        
     else:
-        while suma_en_pantalla != suma:
+        while suma_en_pantalla != resultado_suma:
             suma_en_pantalla = numero_menor + 1
             print(f"{numero_menor} + 1  = {suma_en_pantalla}")
             numero_menor=numero_menor+1
+
+#Para que muestre el resultado final en pantalla
             
-    print(f"El resultado final es:  {suma}")
+    print(f"El resultado final es:  {resultado_suma}")
 
 if __name__ == "__main__":
     prueba()
