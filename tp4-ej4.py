f09dded68c0a0cd7ae4d30f73d0b797088d51c77
Red Crossbones <gastonnehuen@outlook.com>
2021-05-29 18:06:26 -0300

    Ej.4

---
+++ tp4-ej4.py
@@ -0,0 +1,23 @@
+################
+# Gastón Nehuen Rodriguez Valdez - @nehusn
+# Plantilla de ejercicio
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+def prueba():
+    saludo="Comparación de números"
+    saludo_titulo= saludo.upper()
+    print(saludo_titulo+ "\n")
+    numero=int(input("Ingrese un número: "))
+    otro_numero=int(input("Ingrese otro número: "))
+    compara(numero, otro_numero)
+
+def compara(numero, otro_numero):
+    if numero <  otro_numero:
+        print("\n-1")
+    elif (numero == otro_numero):
+        print("\n0")
+    else:
+        print("\n1")
+    
+if __name__ == "__main__":
+    prueba()
