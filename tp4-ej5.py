31c498b9295ff0b9190ede23eb49c6a2948adabb
Red Crossbones <gastonnehuen@outlook.com>
2021-05-29 19:50:03 -0300

    Ej 5

---
+++ tp4-ej5.py
@@ -0,0 +1,23 @@
+################
+# Gastón Nehuen Rodriguez Valdez - @nehusn
+# Plantilla de ejercicio
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+def prueba():
+    saludo="Números positivos y negativos"
+    saludo_titulo= saludo.upper()
+    print(saludo_titulo+"\n")
+    numero=int(input("Ingrese un número: "))
+    
+    signo(numero)
+    
+def signo(numero):
+    if numero < 0:
+        print(f"{numero} es negativo")
+    elif numero==0:
+        print(f"{numero} es cero")
+    else:
+        print(f"{numero} es positivo")
+
+if __name__ == "__main__":
+    prueba()
