4bbc682de4333eb34e7752e48317dde6c904111e
Red Crossbones <gastonnehuen@outlook.com>
2021-05-29 19:49:52 -0300

    Tester

--- tester/tester-de-cosas-de-Martin.py
+++ tester/tester-de-cosas-de-Martin.py
@@ -1,2 +1,15 @@
 print(list(reversed(range(10))))
-#Tester pero de cosas pro
+def ingreso_entero(mensaje):
+    """
+    Esta funcion muestra un mensaje y agrega la # para indicar el ingreso
+    de un número entero.
+    """
+    mensaje="0"
+    ingreso = input(mensaje + " #")
+    try:
+        entero = int(ingreso)
+    except ValueError as err:
+        raise IngresoIncorrecto("No era un número!") from err
+    return entero
+
+ingreso_entero(mensaje)
