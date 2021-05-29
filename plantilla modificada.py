d40649cd0636b23c90e21601f0f0a7e065c4572a
Red Crossbones <gastonnehuen@outlook.com>
2021-05-29 18:07:10 -0300

    Plantilla para uso personal de entregas

---
+++ plantilla modificada.py
@@ -0,0 +1,13 @@
+################
+# Gastón Nehuen Rodriguez Valdez - @nehusn
+# Plantilla de ejercicio
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+def prueba():
+    saludo=" "
+    saludo_titulo= saludo.upper()
+    print(saludo_titulo)
+
+    
+if __name__ == "__main__":
+    prueba()
