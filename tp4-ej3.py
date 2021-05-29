c52d28d3d37473334bdab17361b134f827065a45
Red Crossbones <gastonnehuen@outlook.com>
2021-05-29 17:42:50 -0300

    Ej3

---
+++ tp4-ej3.py
@@ -0,0 +1,18 @@
+################
+# Gastón Nehuen Rodriguez Valdez - @nehusn
+# Plantilla de ejercicio
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+def prueba():
+    print("CONVERTOR DE GRADOS")
+    
+def convertir_a_fahrenheit(centigrados):
+    fahrenheit= (centigrados * 9/5) + 32
+    print(f"Farenheit: {fahrenheit}")
+
+def convertir_a_centigrados(fahrenheit):
+    centigrados= (fahrenheit - 32) * 5/9
+    print(f"Centigrados: {centigrados}")
+    
+if __name__ == "__main__":
+    prueba()
