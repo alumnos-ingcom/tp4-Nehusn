e981b4dbdf0d0db8d5d3b0d1dd247f7670009720
Red Crossbones <gastonnehuen@outlook.com>
2021-05-29 21:11:10 -0300

    Ej.6

---
+++ prueba.py
@@ -0,0 +1,22 @@
+################
+# Gastón Nehuen Rodriguez Valdez - @nehusn
+# Plantilla de ejercicio
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+def prueba():
+    saludo="Máximo / Mínimo "
+    saludo_titulo= saludo.upper()
+    print(saludo_titulo+"\n")
+    lista=[]
+    ingreso=0
+    activo=1
+    while activo == 1:
+        ingreso=input("Ingrese numero: ")
+        if ingreso == int:
+            int(ingreso)
+            lista.append(ingreso)
+        else:
+            activo=0
+            print(lista)
+if __name__ == "__main__":
+    prueba()
---
+++ tp4-ej6.py
@@ -0,0 +1,32 @@
+################
+# Gastón Nehuen Rodriguez Valdez - @nehusn
+# Plantilla de ejercicio
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+def prueba():
+    saludo="Máximo / Mínimo "
+    saludo_titulo= saludo.upper()
+    print(saludo_titulo+"\n")
+    lista=[]
+    ingreso=0
+    
+    while ingreso != "":
+        ingreso=input("Ingrese numero: ")
+        lista.append(ingreso)
+    else:
+        lista.pop()
+        lista.sort()
+        minimo(lista)
+        maximo(lista)
+
+def minimo(lista):
+    minimo=lista[0]
+    print(f"Minimo es: {minimo}")
+
+def maximo(lista):
+    lista.reverse()
+    maximo=lista[0]
+    print(f"Máximo es: {maximo}")
+    
+if __name__ == "__main__":
+    prueba()
