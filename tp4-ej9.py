b8aae922bdbb2c637e0484f20eee03b80ed485c3
Red Crossbones <gastonnehuen@outlook.com>
2021-06-05 17:37:53 -0300

    Create tp4-ej9.py

---
+++ tp4-ej9.py
@@ -0,0 +1,13 @@
+################
+# Gastón Nehuen Rodriguez Valdez - @nehusn
+# Plantilla de ejercicio
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+def prueba():
+    saludo="Factores Primos "
+    saludo_titulo= saludo.upper()
+    print(saludo_titulo+"\n")
+
+    
+if __name__ == "__main__":
+    prueba()
