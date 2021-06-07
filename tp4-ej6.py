e49a545c1e3f99152ce247a999d82a436e57c271
Red Crossbones <gastonnehuen@outlook.com>
2021-06-07 12:28:57 -0300

    Correcciones: elimine los prints y los cambie por returns

--- tp4-ej6.py
+++ tp4-ej6.py
@@ -5,29 +5,42 @@
 ################
 
 def minimo(lista):
-    minimo=lista[0]
-    print(f"Minimo es: {minimo}")
+    for i in range(0, len(lista)):
+        minimo=lista[0]
+        if minimo<=lista[i]:
+            pass
+        else:
+            minimo=lista[i]
+    return minimo
 
 def maximo(lista):
-    lista.reverse()
-    maximo=lista[0]
-    print(f"Máximo es: {maximo}")
+    for i in range(0, len(lista)):
+        maximo=lista[0]
+        if maximo>=lista[i]:
+            pass
+        else:
+            maximo=lista[i]
+    return maximo
+    
 
 def prueba():
     saludo="Máximo / Mínimo "
     saludo_titulo= saludo.upper()
     print(saludo_titulo+"\n")
     lista=[]
-    ingreso=0
-    
+    ingreso=0    
     while ingreso != "":
         ingreso=input("Ingrese numero: ")
         lista.append(ingreso)
     else:
         lista.pop()
-        lista.sort()
         minimo(lista)
         maximo(lista)
+        numero_minimo=minimo(lista)
+        numero_maximo=maximo(lista)
+        print(f"Minimo es: {numero_minimo}")
+        print(f"Máximo es: {numero_maximo}")
+        
 
 if __name__ == "__main__":
     prueba()
