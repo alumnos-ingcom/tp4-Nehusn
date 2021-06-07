87d95fe8ec26aaab6060c16dc664b370aaeed3fb
Red Crossbones <gastonnehuen@outlook.com>
2021-06-07 12:28:30 -0300

    Definicion de funcion saludo c:

--- tp4-ej10.py
+++ tp4-ej10.py
@@ -3,7 +3,11 @@
 # Plantilla de ejercicio
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
-
+def saludo(saludo):
+    saludo="Palíndromo "
+    saludo_titulo= saludo.upper()
+    print(saludo_titulo+"\n")
+    
 def es_palindromo(texto):
     reverse_txt= texto[::-1]
     if texto==reverse_txt:
@@ -12,10 +16,7 @@ def es_palindromo(texto):
         return False
 
 def prueba():
-    saludo="Palíndromo "
-    saludo_titulo= saludo.upper()
-    print(saludo_titulo+"\n")
-    
+    saludo(saludo)
     texto=input("Ingrese texto: ")
     resultado=es_palindromo(texto)
     print(resultado)
