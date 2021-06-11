5f9e25266211d6873e43c08f9a33d0cb30a2c2ab
Red Crossbones <gastonnehuen@outlook.com>
2021-06-11 10:13:50 -0300

    Tratando de hacerlo pero sin exito

--- tp4-ej1.py
+++ tp4-ej1.py
@@ -1,4 +1,32 @@
-#Incompleto por el momento. Come back later  c: # 
-def main():
-    print("buen dia")
-#Still incomplete
+################
+# Gastón Nehuen Rodriguez Valdez - @nehusn
+# Plantilla de ejercicio
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+def numero_entre_valores(numero,valorA,valorB):
+    numero_mayor=0
+    numero_menor=0
+    if valorA < valorB:
+        numero_mayor=valorB
+        numero_menor=valorA
+    else:
+        numero_mayor=valorA
+        numero_menor=valorB
+    if numero >= numero_menor and numero <= numero_mayor:
+        return True
+    else:
+        return False
+def prueba():
+    saludo="Flor "
+    saludo_titulo= saludo.upper()
+    print(saludo_titulo+"\n")
+    valorA=int(input("Ingrese un valor: "))
+    valorB=int(input("Ingrese otro valor: "))
+    numero=int(input("Ingrese un numero entre esos valores: "))
+    numero_entre_valores(numero,valorA,valorB)
+    resultado=numero_entre_valores
+    print(resultado)
+    
+if __name__ == "__main__":
+    prueba()
